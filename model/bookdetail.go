ea5a318ccca4fe76d1381780010d93a3c408912d
yxh <417670161@qq.com>
2020-08-22 23:29:37 +0800

    Complete information classification acquisition.

--- model/bookdetail.go
+++ model/bookdetail.go
@@ -1,10 +1,16 @@
 package model
 
+import "strconv"
+
 type BookDetail struct {
-	Author string
-	Publicer string
+	Author    string
+	Publicer  string
 	Bookpages int
-	Price string
-	Score string
-	Intro string
+	Price     string
+	Score     string
+	Intro     string
+}
+
+func (b BookDetail) String() string {
+	return "作者：" + b.Author + " 出版社：" + b.Publicer + " 书籍页数：" + strconv.Itoa(b.Bookpages) + " 价格：" + b.Price + " 得分：" + b.Score + "\n简介：" + b.Intro
 }
--- parse/parsebookdetail.go
+++ parse/parsebookdetail.go
@@ -1,16 +1,50 @@
 package parse
 
-import "redspider/model"
+import (
+	"redspider/engine"
+	"redspider/model"
+	"regexp"
+	"strconv"
+)
 
-const autoRe = `<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`
-const public = `<span class="pl">出版社:</span>([^<]+)<br/>`
-const pageRe = `<span class="pl">页数:</span>([^<]+)<br/>`
-const priceRe = `<span class="pl">定价:</span>([^<]+)<br/>`
-const scoreRe =`<strong class="ll rating_num " property="v:average">([^<]+)</strong>`
-const introRe = `<div class="intro">[\d\D]*?<p>([^<]+)</p></div>`
+//const autoRe = `<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`
+//const public = `<span class="pl">出版社:</span>([^<]+)<br/>`
+//const pageRe = `<span class="pl">页数:</span>([^<]+)<br/>`
+//const priceRe = `<span class="pl">定价:</span>([^<]+)<br/>`
+//const scoreRe = `<strong class="ll rating_num " property="v:average">([^<]+)</strong>`
+//const introRe = `<div class="intro">[\d\D]*?<p>([^<]+)</p></div>`
 
-func ParseBookDetail(contents []byte)  {
-	bookdetail:=model.BookDetail{}
-	//match:=autoRe.F
+var autoRe = regexp.MustCompile(`<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`)
+var public = regexp.MustCompile(`<span class="pl">出版社:</span>([^<]+)<br/>`)
+var pageRe = regexp.MustCompile(`<span class="pl">页数:</span> ([^<]+)<br/>`)
+var priceRe = regexp.MustCompile(`<span class="pl">定价:</span>([^<]+)<br/>`)
+var scoreRe = regexp.MustCompile(`<strong class="ll rating_num " property="v:average">([^<]+)</strong>`)
+var introRe = regexp.MustCompile(`<div class="intro">[\d\D]*?<p>([^<]+)</p></div>`)
 
+func ParseBookDetail(contents []byte) engine.ParseResult{
+	bookdetail := model.BookDetail{}
+	bookdetail.Author = ExtraString(contents, autoRe)
+	page, err := strconv.Atoi(ExtraString(contents, pageRe))
+	if err == nil {
+		bookdetail.Bookpages = page
+	}
+	bookdetail.Publicer = ExtraString(contents, public)
+	bookdetail.Intro = ExtraString(contents, introRe)
+	bookdetail.Score = ExtraString(contents, scoreRe)
+	bookdetail.Price = ExtraString(contents,priceRe)
+	result := engine.ParseResult{
+		Items: []interface{}{bookdetail},
+	}
+	return result
+
+}
+
+func ExtraString(contents []byte, re *regexp.Regexp) string {
+	match := re.FindSubmatch(contents)
+
+	if len(match) >= 2 {
+		return string(match[1])
+	} else {
+		return ""
+	}
 }
